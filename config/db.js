92ce35608470f77775d147ce82d9695e49b2bab4
khadisssh <mendibajhadisa@gmail.com>
2025-08-04 19:31:02 +0500

    new commit

--- config/db.js
+++ config/db.js
@@ -1,11 +1,8 @@
 const {Pool} = require('pg')
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    password: 'admin',
-    database: 'finalproject_db'
+    connectionString: 'postgresql://khadisha:fqwowbHIwXuOQ6HEVhXsXlKkpkAw8VJi@dpg-d28bgobipnbc739ig2dg-a.oregon-postgres.render.com/finalproject_db_tj8y',
+    ssl: {rejectUnauthorized: false}
 })
 
 module.exports = pool 
